4aedff7faf897a28b1177ff81f0dae07a63c47e3
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-25 23:30:09 +0200

    added recording

--- App.js
+++ App.js
@@ -1,11 +1,87 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Audio } from "expo-av";
 
 export default function App() {
+  const [recording, setRecording] = useState();
+  const [recordings, setRecordings] = useState([]);
+
+  async function startRecording() {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true,
+        });
+        const { recording } = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+        setRecording(recording);
+      }
+    } catch (err) {}
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+
+    await recording.stopAndUnloadAsync();
+    let allRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    allRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI(),
+    });
+
+    setRecordings(allRecordings);
+  }
+
+  function getDurationFormatted(milliseconds) {
+    const minutes = milliseconds / 1000 / 60;
+    const seconds = Math.round((minutes - Math.floor(minutes)) * 60);
+    return seconds < 10
+      ? `${Math.floor(minutes)}:0${seconds}`
+      : `${Math.floor(minutes)}:${seconds}`;
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>
+            Recording #{index + 1} | {recordingLine.duration}
+          </Text>
+          <Button
+            color={"green"}
+            borderRadius={16}
+            onPress={() => recordingLine.sound.replayAsync()}
+            title="Play"
+          ></Button>
+        </View>
+      );
+    });
+  }
+
+  function clearRecordings() {
+    setRecordings([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Audio</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textAudio}>myAudio</Text>
+      <View style={styles.wrapper}>
+        <Button
+          title={recording ? "Stop Recording" : "Start Recording\n"}
+          onPress={recording ? stopRecording : startRecording}
+        />
+        <Button
+          title={recordings.length > 0 ? "\nClear Recordings" : ""}
+          onPress={clearRecordings}
+        />
+      </View>
+
+      {getRecordingLines()}
     </View>
   );
 }
@@ -15,6 +91,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    marginTop: 26,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
     justifyContent: "center",
+    marginLeft: 10,
+    marginRight: 40,
+  },
+
+  fill: {
+    flex: 1,
+    margin: 15,
+    borderRadius: 20,
+  },
+  textAudio: {
+    color: "red",
+  },
+  wrapper: {
+    flexDirection: "row",
+    gap: 10,
   },
 });
